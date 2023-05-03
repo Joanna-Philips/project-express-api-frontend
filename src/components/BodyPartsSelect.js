97ac132e4890a900175c10ce0085c50fd26a1797
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-05-03 20:49:24 +0200

    Display results from select done

--- src/components/BodyPartsSelect.js
+++ src/components/BodyPartsSelect.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux';
+import { Paper } from '@mui/material';
 
 export const BodyPartsSelect = () => {
   const bodyParts = useSelector((store) => store.workouts.bodyPartsSelect)
@@ -8,10 +9,11 @@ export const BodyPartsSelect = () => {
   useEffect(() => {
     fetch(`https://project-express-api-lldotyfewa-lz.a.run.app/workouts?BodyPart=${bodyParts}`)
       .then((res) => res.json())
-      .then((data) => setBodyPartsList(data))
+      .then((data) => {
+        console.log(data);
+        setBodyPartsList(data.body.workoutsData);
+      })
       .catch((error) => alert(error, 'error'))
-    //   .finally(() => setLoading(false));
-    console.log('fetch data bodyPartsSelect', bodyPartsList)
   }, [])
 
   return (
@@ -19,12 +21,12 @@ export const BodyPartsSelect = () => {
       {bodyPartsList.length > 0 && bodyPartsList.map((item) => {
         return (
           <div key={item.Id}>
-            <div>
+            <Paper elevation={3}>
               <p>{item.Title}</p>
               <p>Type: {item.Type}</p>
               <p>Equipment: {item.Equipment}</p>
               <p>Level: {item.Level}</p>
-            </div>
+            </Paper>
           </div>
         )
       })}
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,15 +1,17 @@
+/* eslint-disable max-len */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable operator-linebreak */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Select, MenuItem, InputLabel, FormControl } from '@mui/material';
 import { workouts } from 'reducers/workouts';
 import { BodyPartsSelect } from './BodyPartsSelect';
 
 export const StartPage = () => {
   const dispatch = useDispatch();
-  const [allWorkouts, setAllWorkouts] = useState([])
+  const [allBodyParts, setAllBodyParts] = useState([])
   const bodyParts = useSelector((store) => store.workouts.bodyPartsSelect)
 
   const selectBodyPart = (event) => {
@@ -33,10 +35,8 @@ export const StartPage = () => {
       .then((res) => res.json())
       .then((data) => {
         const uniqueBodyParts = data.body.workoutsData.filter((item, index, self) =>
-          index === self.findIndex((t) => (
-            t.BodyPart === item.BodyPart
-          )));
-        setAllWorkouts(uniqueBodyParts)
+          index === self.findIndex((t) => (t.BodyPart === item.BodyPart)));
+        setAllBodyParts(uniqueBodyParts)
       })
       .catch((error) => alert(error, 'error'))
   }, [])
@@ -44,28 +44,28 @@ export const StartPage = () => {
   return (
 
     <div>
-      {allWorkouts &&
-      <div>
-        <h1>StartPage here</h1>
-        <label hidden htmlFor="body-parts">Select a muscle group to work on:</label>
-        <select
+      {allBodyParts &&
+      <FormControl sx={{ m: 1, minWidth: 200 }}>
+        <InputLabel hidden htmlFor="body-parts">Select a muscle group</InputLabel>
+        <Select
           aria-label="List of body parts"
           value="body-parts"
           name="body-parts"
+          label="Select muscle group"
           onChange={selectBodyPart}>
 
-          <option defaultValue>Select a muscle group</option>
-          {allWorkouts.map((item) => {
+          {/* <MenuItem defaultValue>Select a muscle group</MenuItem> */}
+          {allBodyParts.map((item) => {
             return (
-              <option
+              <MenuItem
                 key={item.Id}
                 value={item.BodyPart}>
                 {item.BodyPart}
-              </option>
+              </MenuItem>
             );
           })}
-        </select>
-      </div>}
+        </Select>
+      </FormControl>}
       {bodyParts.length > 0 ? <BodyPartsSelect /> : ''}
     </div>
   )
