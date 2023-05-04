095e75db9dcec867b7703e283249723a8e797d8f
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-05-04 22:20:47 +0200

    Resized lottie

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
+import styled from 'styled-components';
 
 export const Loader = () => {
   return (
-    <Player
-      src="https://assets9.lottiefiles.com/packages/lf20_ocusgcsp.json"
-      loop
-      autoplay
-      speed={2.5} />
+    <LottieWrapper>
+      <Player
+        height="40vh"
+        width="40vw"
+        src="https://assets9.lottiefiles.com/packages/lf20_ocusgcsp.json"
+        loop
+        autoplay
+        speed={2.5} />
+    </LottieWrapper>
   )
 }
+
+const LottieWrapper = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    `
