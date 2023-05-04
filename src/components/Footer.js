b54a15cd657f11e1882dfa97af9c350e488d7209
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-05-04 22:50:31 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
     <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, color: 'black', height: 0.06 }} elevation={3}>
       <BottomNavigation
         showLabels>
-        <Typography sx={{ p: 1, fontSize: 12 }}> 2023 KEEP FIT - Made by Joanna Philips</Typography>
+        <Typography sx={{ p: 1, fontSize: 12, textAlign: 'center' }}> 2023 KEEP FIT - Made by Joanna Philips <br /> API: https://project-express-api-lldotyfewa-lz.a.run.app/</Typography>
       </BottomNavigation>
     </Paper>
   )
